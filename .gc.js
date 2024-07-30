a27adad0def94999f289f4bb0f8b515d6f687fd1
noureddin <noureddin@protonmail.com>
2024-07-30 03:27:14 +0200

    add url param to disable statistics: nostats

--- .gc.js
+++ .gc.js
@@ -1,3 +1,5 @@
-window.goatcounter = { path: location.href, allow_frame: true }
-// privacy-friendly statistics, no tracking of personal data, no need for GDPR consent; see goatcounter.com
-B.append(make_elem('script', { Dataset: { goatcounter: 'https://ihkam.goatcounter.com/count' }, async: true, src: '//gc.zgo.at/count.js' }))
+if (!(location.search + location.hash).split(/[?&#]/).includes('nostats')) {
+  window.goatcounter = { path: location.href, allow_frame: true }
+  // privacy-friendly statistics, no tracking of personal data, no need for GDPR consent; see goatcounter.com
+  B.append(make_elem('script', { Dataset: { goatcounter: 'https://ihkam.goatcounter.com/count' }, async: true, src: '//gc.zgo.at/count.js' }))
+}
