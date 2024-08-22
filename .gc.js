59c53efb958c69d3570be49fe4e27db8c5139521
noureddin <noureddin@protonmail.com>
2024-08-22 16:36:15 +0300

    put count.js on the same server

--- .gc.js
+++ .gc.js
@@ -1,5 +1,5 @@
 if (!(location.search + location.hash).split(/[?&#]/).includes('nostats')) {
   window.goatcounter = { path: location.href, allow_frame: true }
   // privacy-friendly statistics, no tracking of personal data, no need for GDPR consent; see goatcounter.com
-  B.append(make_elem('script', { Dataset: { goatcounter: 'https://ihkam.goatcounter.com/count' }, async: true, src: '//gc.zgo.at/count.js' }))
+  B.append(make_elem('script', { Dataset: { goatcounter: 'https://ihkam.goatcounter.com/count' }, async: true, src: 'count.js' }))
 }
